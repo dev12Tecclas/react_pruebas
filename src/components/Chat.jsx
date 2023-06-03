212c044f831d60c46e6357cda39a819ba9004b7a
dev13tecclas <133042272+dev13tecclas@users.noreply.github.com>
2023-06-03 10:28:56 -0500

    chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+/* import React from 'react'
 
 export default function Chat() {
   return (
     <div className='chat'>
         <div>
-        <ul></ul>
+        <ul>
+
+        </ul>
 
         </div>
         <div className='formChat'>
@@ -16,4 +18,51 @@ export default function Chat() {
         </div>
     </div>
   )
+} */
+// App.js
+
+import React, { useState } from 'react';
+import './styles.css';
+
+export const  chat = () => {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+
+  const handleSendMessage = (event) => {
+    event.preventDefault();
+    if (message.trim() !== '') {
+      setMessages([...messages, message]);
+      setMessage('');
+    }
+  };
+
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  return (
+    <div className="app">
+      <h1>Chat en React</h1>
+      <div className="chat">
+        <div className="message-list">
+          {messages.map((message, index) => (
+            <div key={index} className="message">
+              {message}
+            </div>
+          ))}
+        </div>
+        <form onSubmit={handleSendMessage} className="message-input">
+          <input
+            type="text"
+            placeholder="Escribe un mensaje..."
+            value={message}
+            onChange={handleChange}
+          />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    </div>
+  );
 }
+
+export default App;
