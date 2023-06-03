42557de6e5061aa4635168775ffff30b314bf691
dev13tecclas <133042272+dev13tecclas@users.noreply.github.com>
2023-06-03 13:55:20 -0500

    rr

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,13 +1,10 @@
-
-
-
 import React, { useState } from 'react';
-// import './styles.css';
 
-
-export const  Chat = () => {
+export const Chat = () => {
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState('');
+  const [username, setUsername] = useState('');
+  const [users, setUsers] = useState([]);
 
   const handleSendMessage = (event) => {
     event.preventDefault();
@@ -21,9 +18,24 @@ export const  Chat = () => {
     setMessage(event.target.value);
   };
 
+  const handleLogin = (event) => {
+    event.preventDefault();
+    if (username.trim() !== '') {
+      setUsers([...users, username]);
+      setUsername('');
+    }
+  };
+
   return (
     <div className="app">
-
+      <div className="sidebar">
+        <h2>Usuarios</h2>
+        <ul>
+          {users.map((user, index) => (
+            <li key={index}>{user}</li>
+          ))}
+        </ul>
+      </div>
       <div className="chat">
         <div className="message-list">
           {messages.map((message, index) => (
@@ -42,7 +54,17 @@ export const  Chat = () => {
           <button type="submit">Enviar</button>
         </form>
       </div>
+      <div className="login">
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Ingresa tu nombre de usuario"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <button type="submit">Iniciar sesión</button>
+        </form>
+      </div>
     </div>
   );
-}
-
+};
